b6a31b52877521a2a9ad757a988e5885f1bc54f4
Matt Schafer <lucidsigma17@gmail.com>
2018-12-04 11:03:41 +1000

    2018-Dec-04
    Solution for 4th of December.

---
+++ 2018-12-04/XORLinkedList.cpp
@@ -0,0 +1,127 @@
+#include <iostream>
+#include <stdexcept>
+
+template<typename T>
+T* XorAddresses(T* previous, T* next)
+{
+	return reinterpret_cast<T*>(reinterpret_cast<int>(previous) ^ reinterpret_cast<int>(next));
+}
+
+template<typename T>
+class XorList
+{
+private:
+	struct Node
+	{
+		T data;
+		Node* xorAddress;
+
+		Node(T data, Node* previous, Node* next)
+			: data(data), xorAddress(XorAddresses(previous, next))
+		{ }
+	};
+
+	Node* head = nullptr;
+	unsigned int size = 0;
+
+public:
+	XorList() = default;
+
+	~XorList()
+	{
+		DestroyNode(nullptr, head);
+	}
+
+	void Append(int data)
+	{
+		if (head == nullptr)
+		{
+			head = new Node(data, nullptr, nullptr);
+		}
+		else
+		{
+			Node* previous = nullptr;
+			Node* current = head;
+			Node* next = XorAddresses(previous, current->xorAddress);
+
+			while (next != nullptr)
+			{
+				previous = current;
+				current = next;
+				next = XorAddresses(previous, current->xorAddress);
+			}
+
+			Node* node = new Node(data, current, nullptr);
+			current->xorAddress = XorAddresses(previous, node);
+		}
+
+		size++;
+	}
+
+	T Get(unsigned int index) const
+	{
+		Node* previous = nullptr;
+		Node* current = head;
+
+		unsigned int iterationCounter = index;
+
+		while ((iterationCounter > 0) && (current != nullptr))
+		{
+			Node* next = XorAddresses(previous, current->xorAddress);
+
+			previous = current;
+			current = next;
+
+			iterationCounter--;
+		}
+
+		if (iterationCounter != 0)
+		{
+			throw std::out_of_range("Index out of range of XOR linked list.");
+		}
+
+		return current->data;
+	}
+
+	T operator [](unsigned int index) const
+	{
+		return Get(index);
+	}
+
+	inline unsigned int Size() const noexcept { return size; }
+
+private:
+	void DestroyNode(Node* previous, Node* current)
+	{
+		if (current == nullptr)
+		{
+			return;
+		}
+		
+		Node* next = XorAddresses(previous, current->xorAddress);
+
+		if (next != nullptr)
+		{
+			DestroyNode(current, next);
+
+			delete next;
+		}
+	}
+};
+
+int main(int argc, char* argv[])
+{
+	XorList<int> xorList;
+
+	xorList.Append(10);
+	xorList.Append(25);
+	xorList.Append(42);
+
+	std::cout << xorList.Get(2) << std::endl;
+	std::cout << xorList[1] << std::endl;
+	//std::cout << xorList[4] << std::endl; // Creates exception
+
+	std::cin.get();
+	
+	return 0;
+}
