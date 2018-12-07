264e832623fda7527a6d2c39b79a86dfb7e14bbf
Matt Schafer <lucidsigma17@gmail.com>
2018-12-07 12:34:33 +1000

    2018-Dec-07
    Solution for the 7th of December.

---
+++ 2018-12/2018-12-07/MaxSumOfNonAdjacentNumbers.cpp
@@ -0,0 +1,34 @@
+#include <algorithm>
+#include <iostream>
+#include <type_traits>
+#include <vector>
+
+template<typename T, typename = std::enable_if_t<std::is_arithmetic_v<T>>>
+T FindMaxNonAdjacentSum(const std::vector<T>& numbers)
+{
+	T sumWithPrevious = numbers[0];
+	T sumWithoutPrevious = 0;
+
+	for (unsigned int i = 1; i < numbers.size(); i++)
+	{
+		T sumWithoutPreviousNew = std::max(sumWithPrevious, sumWithoutPrevious);
+
+		sumWithPrevious = sumWithoutPrevious + numbers[i];
+		sumWithoutPrevious = sumWithoutPreviousNew;
+	}
+
+	return std::max(sumWithPrevious, sumWithoutPrevious);
+}
+
+int main(int argc, char* argv[])
+{
+	std::vector<int> numbers { 2, 4, 6, 2, 5 };
+	std::cout << FindMaxNonAdjacentSum(numbers) << std::endl;
+
+	std::vector<int> moreNumbers { 5, 1, 1, 5 };
+	std::cout << FindMaxNonAdjacentSum(moreNumbers) << std::endl;
+
+	std::cin.get();
+
+	return 0;
+}
