674e33370821432e71f51b39f31823277d755a6c
Matt Schafer <lucidsigma17@gmail.com>
2018-12-16 10:07:53 +1000

    2018-Dec-16
    Solution for the 16th of December, 2018.

---
+++ 2018-12/2018-12-16/MaximumElementInEachSubarray.cpp
@@ -0,0 +1,48 @@
+#include <array>
+#include <deque>
+#include <iostream>
+
+template <unsigned int Size>
+void PrintSubarrayMaximums(const std::array<int, Size> array, unsigned int k) noexcept
+{
+	std::deque<int> elements(k);
+
+	for (unsigned int i = 0; i < k; i++)
+	{
+		while (!elements.empty() && (array[i] >= array[elements.back()]))
+		{
+			elements.pop_back();
+		}
+
+		elements.push_back(i);
+	}
+
+	for (unsigned int i = k; i < array.size(); i++)
+	{
+		std::cout << array[elements.front()] << " ";
+
+		while (!elements.empty() && (elements.front() <= i - k))
+		{
+			elements.pop_front();
+		}
+
+		while (!elements.empty() && (array[i] >= array[elements.back()]))
+		{
+			elements.pop_back();
+		}
+
+		elements.push_back(i);
+	}
+
+	std::cout << array[elements.front()] << std::endl;
+}
+
+int main(int argc, char* argv[])
+{
+	PrintSubarrayMaximums<6>({ 10, 5, 2, 7, 8, 7 }, 3);
+	PrintSubarrayMaximums<10>({ 5, 8, 2, 6, 9, 3, 2, 0, 6, 7 }, 4);
+
+	std::cin.get();
+
+	return 0;
+}
