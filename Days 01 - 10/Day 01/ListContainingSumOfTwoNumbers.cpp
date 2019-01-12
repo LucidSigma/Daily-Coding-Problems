47e262704861e54c915fc4f4808f95e9ea57d394
Matt Schafer <39696157+LucidSigma@users.noreply.github.com>
2019-01-12 11:08:33 +1000

    More efficiency.

--- Days 01 - 10/Day 01/ListContainingSumOfTwoNumbers.cpp
+++ Days 01 - 10/Day 01/ListContainingSumOfTwoNumbers.cpp
@@ -3,7 +3,7 @@
 #include <type_traits>
 #include <vector>
 
-template<typename T, typename = typename std::enable_if<std::is_integral<T>::value>::type>
+template <typename T, typename = typename std::enable_if<std::is_integral<T>::value>::type>
 bool ContainsTwoNumberSum(const std::vector<T>& numberList, T total)
 {
 	std::unordered_set<T> numberHash;
@@ -25,25 +25,10 @@ bool ContainsTwoNumberSum(const std::vector<T>& numberList, T total)
 
 int main(int argc, char* argv[])
 {
-	std::vector<int> numbers { 10, 15, 3, 7, 34, 2, 9, 6, 17, 5, -4 };
+	std::vector<int> numbers { 10, 15, 3, 7 };
 
 	std::cout << std::boolalpha;
-
-	std::cout << ContainsTwoNumberSum(numbers,  1) << std::endl;	// true
-	std::cout << ContainsTwoNumberSum(numbers, 17) << std::endl;	// true
-	std::cout << ContainsTwoNumberSum(numbers, 41) << std::endl;	// true
-	std::cout << ContainsTwoNumberSum(numbers, 29) << std::endl;	// false
-	std::cout << ContainsTwoNumberSum(numbers, 13) << std::endl;	// true
-	std::cout << ContainsTwoNumberSum(numbers, 15) << std::endl;	// true
-	std::cout << ContainsTwoNumberSum(numbers, 64) << std::endl;	// false
-	std::cout << ContainsTwoNumberSum(numbers, 23) << std::endl;	// true
-	std::cout << ContainsTwoNumberSum(numbers,  7) << std::endl;	// true
-	std::cout << ContainsTwoNumberSum(numbers, -1) << std::endl;	// true
-	std::cout << ContainsTwoNumberSum(numbers, 43) << std::endl;	// true
-	std::cout << ContainsTwoNumberSum(numbers, 28) << std::endl;	// false
-	std::cout << ContainsTwoNumberSum(numbers, 16) << std::endl;	// true
-	std::cout << ContainsTwoNumberSum(numbers,  6) << std::endl;	// true
-	std::cout << ContainsTwoNumberSum(numbers, 19) << std::endl;	// true
+	std::cout << ContainsTwoNumberSum(numbers, 17) << "\n";
 
 	std::cin.get();
 
