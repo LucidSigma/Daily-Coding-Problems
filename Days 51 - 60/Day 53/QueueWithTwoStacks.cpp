b1f429b4caab437813e9e3ca335bef377a1756c9
Matt Schafer <lucidsigma17@gmail.com>
2019-01-20 11:14:45 +1000

    Day 53.
    Solution for Day 53.

---
+++ Days 51 - 60/Day 53/QueueWithTwoStacks.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+#include <stack>
+#include <stdexcept>
+
+class Queue
+{
+private:
+	std::stack<int> stackA;
+	std::stack<int> stackB;
+
+public:
+	void Enqueue(int value)
+	{
+		stackA.push(value);
+	}
+
+	void Dequeue()
+	{
+		if (stackA.empty())
+		{
+			return;
+		}
+
+		SwapStacks(stackA, stackB);
+		stackB.pop();
+
+		SwapStacks(stackB, stackA);
+	}
+
+	int Peek()
+	{
+		if (stackA.empty())
+		{
+			throw std::out_of_range("Queue is empty.");
+		}
+
+		SwapStacks(stackA, stackB);
+
+		int value = stackB.top();
+
+		SwapStacks(stackB, stackA);
+
+		return value;
+	}
+
+private:
+	void SwapStacks(std::stack<int>& a, std::stack<int>& b)
+	{
+		while (!a.empty())
+		{
+			b.push(a.top());
+			a.pop();
+		}
+	}
+};
+
+int main(int argc, const char* argv[])
+{
+	Queue queue;
+
+	queue.Enqueue(5);
+	queue.Enqueue(2);
+	queue.Enqueue(7);
+	std::cout << queue.Peek() << "\n";
+
+	queue.Dequeue();
+	std::cout << queue.Peek() << "\n";
+
+	std::cin.get();
+
+	return 0;
+}
