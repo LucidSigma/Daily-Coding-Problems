7cfe7928553db69f7144f99bf452ceb4c5a12829
Matt Schafer <lucidsigma17@gmail.com>
2019-01-28 18:57:56 +1000

    Day 61.
    Solution for Day 61.

---
+++ Days 61 - 70/Day 61/Exponentiation.cpp
@@ -0,0 +1,57 @@
+#include <cmath>
+#include <iostream>
+#include <stdexcept>
+
+float Pow(int x, int y)
+{
+	if (x == 0 && y == 0)
+	{
+		throw std::invalid_argument("Undefined operation.");
+	}
+
+	if (y == 0)
+	{
+		return 1;
+	}
+
+	unsigned int positiveY = std::abs(y);
+	unsigned int currentPower = 1;
+	unsigned int previousPower = 0;
+
+	float previousResult = 0;
+	float result = x;
+
+	while (currentPower <= positiveY)
+	{
+		previousResult = result;
+		previousPower = currentPower;
+		result *= result;
+		currentPower <<= 1;
+	}
+
+	previousResult *= Pow(x, positiveY - previousPower);
+
+	return y != positiveY ? 1.0f / previousResult : previousResult;
+}
+
+int main(int argc, char* argv[])
+{
+	std::cout << Pow(2, 10) << "\n";
+	std::cout << Pow(2, -10) << "\n";
+	std::cout << Pow(10, 3) << "\n";
+	std::cout << Pow(10, -3) << "\n";
+	std::cout << Pow(5, 0) << "\n";
+
+	try
+	{
+		std::cout << Pow(0, 0) << "\n";
+	}
+	catch (const std::invalid_argument&)
+	{
+		std::cout << "0 ^ 0 is undefined.\n";
+	}
+
+	std::cin.get();
+
+	return 0;
+}
