75a87cdb07e72abd022430dc05b3362b2ae46596
Matt Schafer <lucidsigma17@gmail.com>
2019-03-28 10:59:31 +1000

    Day 120.
    Added solution for Day 120.

---
+++ Days 111 - 120/Day 120/DoubleInstancedSingleton.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+
+class DoubleSingleton final
+{
+private:
+	DoubleSingleton() noexcept = default;
+	~DoubleSingleton() noexcept = default;
+
+public:
+	DoubleSingleton(const DoubleSingleton&) noexcept = delete;
+	DoubleSingleton(DoubleSingleton&&) noexcept = delete;
+
+	DoubleSingleton& operator =(const DoubleSingleton&) = delete;
+	DoubleSingleton& operator =(DoubleSingleton&&) = delete;
+
+	static DoubleSingleton& GetInstance() noexcept
+	{
+		static DoubleSingleton instanceA;
+		static DoubleSingleton instanceB;
+		static bool instanceDecider = false;
+
+		instanceDecider = !instanceDecider;
+
+		return instanceDecider ? instanceA : instanceB;
+	}
+};
+
+int main(int argc, char* argv[])
+{
+	std::cout << &DoubleSingleton::GetInstance() << "\n";	// Instance 1
+	std::cout << &DoubleSingleton::GetInstance() << "\n";	// Instance 2
+	std::cout << &DoubleSingleton::GetInstance() << "\n";	// Instance 1
+	std::cout << &DoubleSingleton::GetInstance() << "\n";	// Instance 2
+
+	std::cin.get();
+
+	return 0;
+}
