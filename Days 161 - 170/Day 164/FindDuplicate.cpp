9c7530be7d241e525173d6754768367704076c1e
Matt Schafer <lucidsigma17@gmail.com>
2019-05-11 10:27:34 +1000

    Day 164.
    Added solution for Day 164.

---
+++ Days 161 - 170/Day 164/FindDuplicate.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <iterator>
+#include <numeric>
+#include <vector>
+
+inline unsigned int FindDuplicate(const std::vector<unsigned int>& numbers) noexcept
+{
+	unsigned int sumWithoutDuplicate = numbers.size() * (numbers.size() - 1) / 2;
+	unsigned int sumWithDuplicate = std::accumulate(std::cbegin(numbers), std::cend(numbers), 0);
+
+	return sumWithDuplicate - sumWithoutDuplicate;
+}
+
+int main(int argc, char* argv[])
+{
+	std::cout << FindDuplicate({ 1, 2, 3, 4, 4, 5 }) << "\n";
+	std::cout << FindDuplicate({ 1, 2, 3, 4, 5, 6, 7, 7, 8, 9, 10 }) << "\n";
+
+	std::cin.get();
+
+	return 0;
+}
