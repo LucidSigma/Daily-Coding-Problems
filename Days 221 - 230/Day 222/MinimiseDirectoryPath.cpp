ad850e076869a9101dfafaee90637b283bf79dc5
Matt Schafer <lucidsigma17@gmail.com>
2019-07-08 11:45:33 +1000

    Day 222.
    Added solution for Day 222.

---
+++ Days 221 - 230/Day 222/MinimiseDirectoryPath.cpp
@@ -0,0 +1,64 @@
+#include <cstdlib>
+#include <iostream>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <vector>
+
+std::vector<std::string> SplitString(const std::string& text, const char delimiter = ' ')
+{
+	std::string currentToken;
+	std::istringstream reader(text);
+
+	std::vector<std::string> tokens;
+
+	while (std::getline(reader, currentToken, delimiter))
+	{
+		tokens.push_back(currentToken);
+	}
+
+	return tokens;
+}
+
+std::string MinimisePath(const std::string& path) noexcept
+{
+	static constexpr char DirectorySeparator = '/';
+	std::stack<std::string> directoryStack;
+	const std::vector<std::string> directories = SplitString(path, DirectorySeparator);
+
+	for (const auto& directory : directories)
+	{
+		static const std::string currentDirectory = ".";
+		static const std::string previousDirectory = "..";
+	
+		if (directory == previousDirectory && !directoryStack.empty())
+		{
+			directoryStack.pop();
+		}
+		else if (directory != currentDirectory && !directory.empty())
+		{
+			directoryStack.push(directory);
+		}
+	}
+
+	std::string minimisedDirectory;
+
+	while (!directoryStack.empty())
+	{
+		minimisedDirectory = DirectorySeparator + directoryStack.top() + minimisedDirectory;
+		directoryStack.pop();
+	}
+
+	minimisedDirectory += DirectorySeparator;
+
+	return minimisedDirectory;
+}
+
+int main(int argc, char* argv[])
+{
+	std::cout << MinimisePath("/usr/bin/../bin/./scripts/../") << "\n";
+
+	std::cin.get();
+
+	return EXIT_SUCCESS;
+}
