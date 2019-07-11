d85c8897b2d399a29b2ed5d8c7c31681e5049549
Matt Schafer <39696157+LucidSigma@users.noreply.github.com>
2019-07-11 11:47:59 +1000

    Made element-based over index-based.
    Now returns the element instead of the index.

--- Days 221 - 230/Day 225/LastExecutedPrisoner.cpp
+++ Days 221 - 230/Day 225/LastExecutedPrisoner.cpp
@@ -5,18 +5,18 @@
 #include <numeric>
 #include <vector>
 
-size_t GetLastSurvivor(const unsigned int n, const unsigned int k) noexcept
+unsigned int GetLastSurvivor(const unsigned int n, const unsigned int k) noexcept
 {
-	size_t lastSurivorIndex = 0;
+	unsigned int lastSurivor = 0;
 	size_t currentExecutionIndex = 0;
 
 	std::vector<unsigned int> prisoners(n);
-	std::iota(std::begin(prisoners), std::end(prisoners), 0);
+	std::iota(std::begin(prisoners), std::end(prisoners), 1);
 
 	while (!prisoners.empty())
 	{
 		currentExecutionIndex = (currentExecutionIndex + k - 1) % prisoners.size();
-		lastSurivorIndex = prisoners[currentExecutionIndex];
+		lastSurivor = prisoners[currentExecutionIndex];
 		
 		std::vector<unsigned int> newPrisoners(std::cbegin(prisoners), std::cbegin(prisoners) + currentExecutionIndex);
 		newPrisoners.insert(std::cend(newPrisoners), std::cbegin(prisoners) + currentExecutionIndex + 1, std::cend(prisoners));
@@ -24,7 +24,7 @@ size_t GetLastSurvivor(const unsigned int n, const unsigned int k) noexcept
 		prisoners = newPrisoners;
 	}
 
-	return lastSurivorIndex;
+	return lastSurivor;
 }
 
 int main(int argc, char* argv[])
