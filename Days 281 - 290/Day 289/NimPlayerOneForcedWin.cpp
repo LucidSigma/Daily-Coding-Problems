7170c9cc39c325fc7eb5222b754ab6a737d8bf95
Matt Schafer <lucidsigma17@gmail.com>
2019-09-13 09:57:11 +1000

    Removed unneeded reference wrapper.
    Removed unneeded reference wrapper in array.

--- Days 281 - 290/Day 289/NimPlayerOneForcedWin.cpp
+++ Days 281 - 290/Day 289/NimPlayerOneForcedWin.cpp
@@ -1,14 +1,13 @@
 #include <array>
 #include <cstddef>
 #include <cstdlib>
-#include <functional>
 #include <iostream>
 #include <tuple>
 
 bool PlayerOneForcedWin(const std::tuple<unsigned int, unsigned int, unsigned int>& startingHeaps)
 {
 	const auto [a, b, c] = startingHeaps;
-	const std::array<std::reference_wrapper<const unsigned int>, 3> heapArray{ a, b, c };
+	const std::array<unsigned int, 3> heapArray{ a, b, c };
 	unsigned int flag = a;
 
 	for (size_t i = 1; i < heapArray.size(); ++i)
